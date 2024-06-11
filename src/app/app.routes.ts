69d22e56650da4f57d6da87bc23f2512d14a415f
unknown <akuma2504@gmail.com>
2024-06-11 18:04:01 -0400

    primer problema resuelto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RrhhComponent } from './rrhh/rrhh.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component:HomeComponent},
+    {path:'rrhh', component:RrhhComponent},
+    {path:'', redirectTo:'home',pathMatch:'full'}
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+//Importacion de modulos de la vista home
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card"
+
+//importacion de router para las rutas o redireccion
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatCardModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  constructor(private router: Router){}
+
+  gotoHome(){
+     console.log(HomeComponent)
+  }
+
+  gotoEstructura(){
+    this.router.navigateByUrl('estructura')
+  }
+
+  gotoRRHH(){
+    this.router.navigateByUrl('rrhh')
+  }
+
+  gotoFasmij(){
+    this.router.navigateByUrl('fasmij')
+  }
+  
+  gotoPhone(){
+    this.router.navigateByUrl('phone')
+  }
+
+  gotoManual(){
+    this.router.navigateByUrl('manual')
+  }
+
+  gotoForms(){
+    this.router.navigateByUrl('forms')
+  }
+}
