13cedf6605fa75d9e6151efcbcd10de1506f1b36
luis gonzales <akuma2504@gmail.com>
2024-06-29 17:39:10 -0400

    cambios de ultimo momento

--- src/app/forms/modals/modals.component.ts
+++ src/app/forms/modals/modals.component.ts
@@ -1,15 +1,21 @@
 import { Component, inject, ChangeDetectionStrategy} from '@angular/core';
+
+// vistas de la pagina 
 import { InstructivoComponent } from './instructivo/instructivo.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialog } from '@angular/material/dialog';
 import { MemoComponent } from './memo/memo.component';
 import { MinutaComponent } from './minuta/minuta.component';
 import { OficioComponent } from './oficio/oficio.component';
 
+// componententes y funcionalidad de la pagina
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+
+
 @Component({
   selector: 'app-modals',
   standalone: true,
-  imports: [ MatButtonModule],
+  imports: [ MatButtonModule, MatCardModule],
   templateUrl: './modals.component.html',
   styleUrl: './modals.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
