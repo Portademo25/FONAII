22918defe4fee36f0d45c45353b752348cc66a0c
luis gonzales <akuma2504@gmail.com>
2024-06-29 20:02:28 -0400

    cambios de ultimo momento3

--- src/app/fasmij/fasmij.component.ts
+++ src/app/fasmij/fasmij.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-fasmij',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './fasmij.component.html',
   styleUrl: './fasmij.component.scss'
 })
 export class FasmijComponent {
 
+  constructor(private router: Router){}
+
+  gotoHome(){
+    this.router.navigateByUrl('home')
+  }
+
+  gotoRRHH(){
+    this.router.navigateByUrl('rrhh')
+  }
+
+  gotoEstructura(){
+    this.router.navigateByUrl('estructura')
+  }
+
+  gotoFasmij(){
+    this.router.navigateByUrl('fasmij')
+  }
+
+  gotoManual(){
+    this.router.navigateByUrl('manual')
+  }
+
+  gotoForms(){
+    this.router.navigateByUrl('forms')
+  }
+
+  gotoPhone(){
+    this.router.navigateByUrl('phone')
+  }
 }
