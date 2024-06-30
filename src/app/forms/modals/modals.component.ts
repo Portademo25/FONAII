ab3de99cd64f2127d1cfd4ab950f79e3863e0ca0
luis gonzales <akuma2504@gmail.com>
2024-06-30 01:25:14 -0400

    modificaciones de modal con icons

--- src/app/forms/modals/modals.component.ts
+++ src/app/forms/modals/modals.component.ts
@@ -3,6 +3,8 @@ import { InstructivoComponent } from './instructivo/instructivo.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MemoComponent } from './memo/memo.component';
+import { MinutaComponent } from './minuta/minuta.component';
+import { OficioComponent } from './oficio/oficio.component';
 
 @Component({
   selector: 'app-modals',
@@ -27,6 +29,22 @@ export class ModalsComponent {
 
   openMemo(enterAnimationDuration: string, exitAnimationDuration: string): void {
     this.dialog.open(MemoComponent, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  openMinuta(enterAnimationDuration: string, exitAnimationDuration: string): void{
+    this.dialog.open(MinutaComponent,{
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  openOficio(enterAnimationDuration:string, exitAnimationDuration:string): void{
+    this.dialog.open(OficioComponent,{
       width: '250px',
       enterAnimationDuration,
       exitAnimationDuration,
--- src/app/forms/modals/oficio/oficio.component.ts
+++ src/app/forms/modals/oficio/oficio.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-oficio',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle ],
   templateUrl: './oficio.component.html',
-  styleUrl: './oficio.component.scss'
+  styleUrl: './oficio.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OficioComponent {
-
+  readonly dialogRef = inject(MatDialogRef<OficioComponent>);
 }
