a79a824e04c4933e1dfaa380d0e7cf9e8de9a235
unknown <akuma2504@gmail.com>
2024-07-02 16:04:13 -0400

    configuracion de modal

--- src/app/rrhh/rrhh.component.ts
+++ src/app/rrhh/rrhh.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-rrhh',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './rrhh.component.html',
   styleUrl: './rrhh.component.scss'
 })
 export class RrhhComponent {
+ constructor (private router: Router){}
+
+ gotoHome(){
+  this.router.navigateByUrl('home')
+ }
+
+ gotoEstructura(){
+  this.router.navigateByUrl('estructura')
+ }
+ gotoRRHH(){
+  this.router.navigateByUrl('rrhh')
+ }
+ 
+ gotoFasmij(){
+  this.router.navigateByUrl('fasmij')
+ }
+
+ gotoForms(){
+  this.router.navigateByUrl('forms')
+ }
+
+ gotoPhone(){
+  this.router.navigateByUrl('phone')
+ }
 
 }
